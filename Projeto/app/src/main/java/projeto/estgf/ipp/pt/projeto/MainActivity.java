15eb8f119d7556bc8ea7858a1da6d8cb93f26d36
alexandrafm <alexandra_fmonteiro@live.com.pt>
2018-11-30 13:23:08 +0000

    Commit incial do projeto com alguns layouts já feitos

---
+++ Projeto/app/src/main/java/projeto/estgf/ipp/pt/projeto/MainActivity.java
@@ -0,0 +1,48 @@
+package projeto.estgf.ipp.pt.projeto;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void showDatePickerDialog(View v) {
+        Calendar cal = Calendar.getInstance();
+        AlertDialog newFragment = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar cal = Calendar.getInstance();
+                        cal.set(Calendar.YEAR, year);
+                        cal.set(Calendar.MONTH, monthOfYear);
+                        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        DateFormat sdf = SimpleDateFormat.getDateInstance();
+                        EditText departureDate = (EditText) findViewById(R.id.editTextDepartureDate);
+                        departureDate.setText(sdf.format(cal.getTime()));
+                    }
+                }, cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        newFragment.show();
+
+    }
+}
