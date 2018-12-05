4209bcf692aa549b475f81d66947269af3e1c44c
AndreiaFilipa <AndreiaFilipa>
2018-12-05 13:29:22 +0000

    Criacao do layout na tab Ida e Regresso relativos aos voos

--- Projeto/app/src/main/java/projeto/estgf/ipp/pt/projeto/TabOne.java
+++ Projeto/app/src/main/java/projeto/estgf/ipp/pt/projeto/TabOne.java
@@ -1,22 +1,83 @@
 package projeto.estgf.ipp.pt.projeto;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
-public class TabOne extends Fragment {
+public class TabOne extends Fragment implements View.OnClickListener {
+    private Button temp;
+    private Button temp1;
+    private Context context;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.context = getActivity();
+
+    }
+
+
+    public void showDatePickerDialog(View v, final int TextViewID) {
+        Calendar cal = Calendar.getInstance();
+        AlertDialog newFragment = new DatePickerDialog(context,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar cal = Calendar.getInstance();
+                        cal.set(Calendar.YEAR, year);
+                        cal.set(Calendar.MONTH, monthOfYear);
+                        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        DateFormat sdf = SimpleDateFormat.getDateInstance();
+                        EditText departureDate = (EditText) ((Activity)context).findViewById(TextViewID);
+                        departureDate.setText(sdf.format(cal.getTime()));
+                    }
+                }, cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        newFragment.show();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.buttonDateDialogDepartureVoo){
+            showDatePickerDialog(v,R.id.editTextDepartureDateVoo);
+        }else if(v.getId() == R.id.buttonDateDialogArriveVoo){
+            showDatePickerDialog(v, R.id.editTextArriveDateVoo);
+        }
+    }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.tab_one, container, false);
+
+
+        temp = (Button) view.findViewById(R.id.buttonDateDialogDepartureVoo);
+        temp.setOnClickListener(this);
+        temp1 = (Button) view.findViewById(R.id.buttonDateDialogArriveVoo);
+        temp1.setOnClickListener(this);
+
         return view;
     }
 
 
 }
+
