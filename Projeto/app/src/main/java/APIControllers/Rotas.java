1db462b0a8df77fa59fdebde88262c299e4bbc97
AndreiaFilipa <AndreiaFilipa>
2018-12-14 16:04:30 +0000

    Criacao do layout da pesquisa voos

--- Projeto/app/src/main/java/APIControllers/Rotas.java
+++ Projeto/app/src/main/java/APIControllers/Rotas.java
@@ -1,5 +1,9 @@
 package APIControllers;
 
+import java.util.List;
+
+import projeto.estgf.ipp.pt.projeto.Data;
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.Query;
